1e3577cf08db44b0adae1d5d058d9f985a5cb83b
Your Name <you@example.com>
2022-01-24 00:39:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = '';
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+          key:alanKey
+      })  
+    },[])
+    return (
+        <div>
+            <h1>Alan AI new Application</h1>
+        </div>
+    )
 }
-
-export default App;
+export default App; 
