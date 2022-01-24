1761b7010ebc0da762318ec17d48d7ae13e3ea72
Your Name <you@example.com>
2022-01-24 02:02:27 +0200

    connecting Alan AI inreact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React,{useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-const alanKey = '';
+const alanKey =
+  "c706e12eea7a5fc570f03bd147584c052e956eca572e1d8b807a3e2338fdd0dc/stage";
+  
 const App = () => {
     useEffect(() => {
         alanBtn({
-          key:alanKey
+            key: alanKey,
+            onCommand: ({command,articles}) => {
+                if (command === 'newHeadlines') {
+                    console.log(articles);
+                }
+            }
       })  
     },[])
     return (
