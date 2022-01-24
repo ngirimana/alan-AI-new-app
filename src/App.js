b4b070939a682d1c030aca081310e0fa537db628
Your Name <you@example.com>
2022-01-24 23:53:00 +0200

    calling command in frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React,{useEffect} from "react";
-import alanBtn from "@alan-ai/alan-sdk-web";
-
-const alanKey =
-  "c706e12eea7a5fc570f03bd147584c052e956eca572e1d8b807a3e2338fdd0dc/stage";
-  
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+import logo from '../src/images/logo_con.gif'
 const App = () => {
-    useEffect(() => {
-        alanBtn({
-            key: alanKey,
-            onCommand: ({command,articles}) => {
-                if (command === 'newHeadlines') {
-                    console.log(articles);
-                }
-            }
-      })  
-    },[])
-    return (
-        <div>
-            <h1>Alan AI new Application</h1>
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: "c706e12eea7a5fc570f03bd147584c052e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img
+          src={logo}
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a
+              className={classes.link}
+              href="https://www.linkedin.com/in/ngirimana-schadrack-650507162/"
+            >
+              {" "}
+              NGIRIMANA Schadrack
+            </a>{" "}
+            -
+            <a className={classes.link} href="https://github.com/ngirimana">
+              {" "}
+              ngirimana-github
+            </a>
+          </Typography>
         </div>
-    )
-}
-export default App; 
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
